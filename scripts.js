3cbeaba45a8e551211a522d69ac6df1aed755c42
Cameron Laury <24402409+Cammylaury@users.noreply.github.com>
2024-03-15 12:55:51 -0500

    fixing JS logic that was broken

--- scripts.js
+++ scripts.js
@@ -1,41 +1,27 @@
 console.log("Hello World!");
 
 const options = ["rock", "paper", "scissors"]
-const playerSelection = "rock"
+const playerSelection = "scissors"
 const computerSelection = getComputerChoice()
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)]
     return choice;
 }
-getComputerChoice()
 
-function checkWinner(playerSelection, comuterSelection) {
-    if (playerSelection == computerSelection) {
-        return "It's a tie!";
-    }
-
-    else if(
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
-    ){
-        return "Player";
-    }
-    else {
-        return "Computer";
-    }
-}
 function playRound(playerSelection, computerSelection) {
-    const result = checkWinner(playerSelection, computerSelection);
-    if (result == "Tie"){
+    if (playerSelection === computerSelection) {
         return "It's a tie!"
-    }
-    else if (result == "Plater"){
-        return `You Win! ${playerSelection} beats ${computerSelection}`
-    }
-    else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors' ) ||
+        (playerSelection === 'scissors' && computerSelection === 'paper') ||
+        (playerSelection === 'paper' && computerSelection === 'rock')
+    ) {
+        return "Player wins!"
+    } else {
+        return "You Lose! The computer won!"
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+let result = playRound(computerSelection, computerSelection);
+
+console.log(result);
