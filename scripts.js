c6f340d2a12790b0ae0c8dafc5dcb19d9879eefc
Cameron Laury <24402409+Cammylaury@users.noreply.github.com>
2024-03-15 22:46:28 -0500

    Added more logging in the console and connected a button to JS

--- scripts.js
+++ scripts.js
@@ -77,5 +77,3 @@ function logRound(playerChoice, computerChoice, winner, round) {
     console.log(winner, 'Won the round!')
     console.log('--------------------------------------------')
 }
-
-game();
