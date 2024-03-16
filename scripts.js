db283fa3d24fa5888612343e1d153ceccbab6675
Cameron Laury <24402409+Cammylaury@users.noreply.github.com>
2024-03-15 22:18:40 -0500

    Added check for player input

--- scripts.js
+++ scripts.js
@@ -13,12 +13,27 @@ function playRound() {
 
 function playerChoice() {
     let input = prompt('Type Rock, Paper, or Scissors');
+    while(input == null) {
+        input = prompt('Type Rock, Paper, or Scissors');
+    }
     input = input.toLowerCase();
-    console.log(input)
+    let check = validateInput(input)
+    if(check == true){
+        console.log(input)
+    }
+    // console.log(input)
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function validateInput(choice) {
+    if(choices.includes(choice)) {
+        return true
+    } else {
+        return false
+    }
 }
 
 game();
